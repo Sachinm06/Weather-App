6b0c6db7e92ecb4e93cd7fb01f43c0ee231ec69c
Sachin M Sibichan <sachinm06@aol.com>
2023-02-25 00:16:25 +0530

    new changes

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-
-fetch('https://api.openweathermap.org/data/2.5/weather?q=calicut&appid=b41ec3be35c7dac8aabbc21ba253137a')
-
-
-function searchbtn() {
+function cityData() {
     citysearch = city.value
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citysearch}&appid=b41ec3be35c7dac8aabbc21ba253137a`)
+    .then(data=>data.json()).then(data=>displayData(data))
+
+
+}
+
+function displayData(cityData){
+
+cityName=cityData.name
+weather=cityData.weather[0].description
+temp=((cityData.main.temp)-273.15).toFixed()
+tempMin=((cityData.main.temp_min)-273.15).toFixed()
+tempMax=((cityData.main.temp_max)-273.15).toFixed()
+feels_like=((cityData.main.feels_like)-273.15).toFixed()
+
+result.innerHTML=`
+<h2 class="city"><i class="fa-solid fa-location-dot"></i>  ${cityName}</h2> 
+<h1 class="temp">${temp}° </h1>
+<h2 class="weather">${weather}</h2>
+<h4 class="feels">Feels like ${feels_like}°</h4>
+<h4 class="mintemp">min${tempMin}°</h4>
+<h4 class="maxtemp">max${tempMax}°</h4>
+`
 
 }
